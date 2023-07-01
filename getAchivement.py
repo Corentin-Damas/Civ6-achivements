02070aa2ca76d972faaa64898a2c9c792b2e67dd
Nazemrap <corentin.damas@gmail.com>
2023-07-01 09:48:58 +0200

    update  get_leaderTable + general architecture

--- getAchivement.py
+++ getAchivement.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import os
 
-dt = pd.read_csv("leaderTable.csv", index_col=False)
-URL = "https://civilization.fandom.com/wiki/"
-
-# pre format all links
-dt['link'] = URL + dt['Leader'].str.replace(' ','_')+"_(Civ6)"
-
 file_path = os.path.dirname(__file__)
 file_name = "leaderTable.csv"
 full_path = os.path.join(file_path, file_name)
 
-dt.to_csv(full_path, index=False)
+
+dt = pd.read_csv("leaderTable.csv", index_col=False)
+
+
+pd.read_html(dt['link'][0])
+
+
+# Save the datas on the db
+# dt.to_csv(full_path, index=False)
+
--- main.py
+++ main.py
@@ -2,34 +2,50 @@ import requests
 import json
 import os
 from utilities.steam_api import get_game_data
+from utilities.get_leaderTable import  getGeneralLeaderInfo
 import pandas as pd
 
 file_path = os.path.dirname(__file__)
 file_name = "game_data.json"
 full_path = os.path.join(file_path, file_name)
 
-# check if the data already exist, if it doesn't exist will go fetch them
+# Create the Achivement data or update from Steam
 if not os.path.isfile(full_path):
-    print("apikey used")
-    get_game_data()  ## Function in utilities
+    print("Collecting the datas")
+    get_game_data()  ## Crate the Achivement datas
+else:
+    upDate = input('Do You want to update your datas ? (y,n)')
+    upDate.lower()
+    if upDate == ('yes' or 'y'):
+        get_game_data() ## Update Achivement Datas
 
+
+# Read the Datas
 with open("game_data.json") as openfile:
     data = json.load(openfile)
 
-data_achivement = data["Sid Meier's Civilization VI"]  ## break key value pair
+# JSON is currently { game_name : {Data}} we want to work only on the datas
+data_achivement = data["Sid Meier's Civilization VI"]
 
-df_full = pd.json_normalize(data_achivement)
-df_refactored = df_full.drop(columns=["apiname", "unlocktime"])
+df_full = pd.json_normalize(data_achivement) # Refactor json to pd dataframe
+df_refactored = df_full.drop(columns=["apiname", "unlocktime"]) # Only keep "achived, name, description" cols
 
 print(df_refactored.head(), df_refactored.columns, df_refactored.info())
-
 print(df_refactored["achieved"].value_counts())
 
-### Untile here / get data and achived success;
+# ------ Get All Civilization Datas from Civ 6 wiki ------
+## Make sur to get the Civilization datas exist
+file_leader = 'leaderTable.csv'
+full_path_leader = os.path.join(file_path, file_leader)
+if not os.path.isfile(full_path_leader):
+    getGeneralLeaderInfo()
+
 
 ### TO DO
-## go to 'https://civilization.fandom.com/wiki/Leaders_(Civ6)'
-## [df New Columns 'leader'] - And for scrap for each leader take the "related achievement"
+
+
+## For each leader take the "related achievement"
+
 ## For each link the leader to the success in the df (some success can be done by different leaders)
 ## [df New Columns 'conditions'] for an achivement some elements might be in the game (artist / wonder)
 ## for each condition (hyper-link) add this element to the condition columns
